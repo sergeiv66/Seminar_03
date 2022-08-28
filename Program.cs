aeeeb4fb34938abff2114c5be915f30f1acae6de
Serghei <sergeiv66@mail.ru>
2022-08-28 21:41:12 +0300

    A task 21

--- Program.cs
+++ Program.cs
@@ -5,6 +5,30 @@
 //12821 -> да
 //23432 -> да
 
-Console.WriteLine("Введите пятизначное число");
-string n = Convert.ToString(Console.ReadLine());
+//Console.WriteLine("Введите пятизначное число");
+//string number = Convert.ToString(Console.ReadLine());
 
+//if ((number[0] == number[4]) && (number[1]==number[3])) Console.WriteLine("yes");
+//else Console.WriteLine("no");
+
+
+//Задача 21
+//Напишите программу, которая принимает на вход координаты двух точек и
+//находит расстояние между ними в 3D пространстве.
+
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите координату первой точки");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату второй точки");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double d = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2));
+
+Console.WriteLine(Math.Round(d,2));
